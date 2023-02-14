e16f16188b7a6a5ab0089b604c906eb829e246aa
Rupesh Tiwari <rupeshti@amazon.com>
2023-02-14 13:26:14 -0500

    updated id of input

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ class App extends Component {
             autoFocus={true}
             type='text'
             name='searchQuery'
+            id='searchQuery'
             placeholder='enter desired text'
           />
 
